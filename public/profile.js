afa22194198c030e50ea94cf419a07b64166e6f3
bownejp20 <jbowne24@gmail.com>
2023-05-04 11:24:02 -0400

    updated the questions to only 5

--- public/profile.js
+++ public/profile.js
@@ -18,65 +18,231 @@ var resultOptions = [
   }
 ];
 
-var quizSteps = $('#quizzie .quiz-step'), totalScore = 0;
-quizSteps.each(function () {
-  var currentStep = $(this), ansOpts = currentStep.children('.quiz-answer');
-  ansOpts.each(function () {
-      var eachOpt = $(this);
-      eachOpt[0].addEventListener('click', check, false);
-      function check() {
 
-          var $this = $(this), value = $this.attr('data-quizIndex'), answerScore = parseInt(value);
-          if (currentStep.children('.active').length > 0) {
-              var wasActive = currentStep.children('.active'), oldScoreValue = wasActive.attr('data-quizIndex'), oldScore = parseInt(oldScoreValue);
-              currentStep.children('.active').removeClass('active');
-              $this.addClass('active');
-              totalScore -= oldScoreValue;
-              totalScore += answerScore;
-              calcResults(totalScore);
-          } else {
-              $this.addClass('active');
-              totalScore += answerScore;
-              calcResults(totalScore);
-              updateStep(currentStep);
-          }
-      }
-  });
-});
-function updateStep(currentStep) {
-  if (currentStep.hasClass('current')) {
-      currentStep.removeClass('current');
-      currentStep.next().addClass('current');
-  }
-}
-function calcResults(totalScore) {
-  if (quizSteps.find('.active').length == quizSteps.length) {
-      var resultsTitle = $('#results h1'), resultsDesc = $('#results .desc');
-      var lowestScoreArray = $('#quizzie .low-value').map(function () {
-          return $(this).attr('data-quizIndex');
-      });
-      var minScore = 0;
-      for (var i = 0; i < lowestScoreArray.length; i++) {
-          minScore += lowestScoreArray[i];
-      }
-      var highestScoreArray = $('#quizzie .high-value').map(function () {
-          return $(this).attr('data-quizIndex');
-      });
-      var maxScore = 0;
-      for (var i = 0; i < highestScoreArray.length; i++) {
-          maxScore += highestScoreArray[i];
-      }
-      var range = maxScore - minScore;
-      var numResults = resultOptions.length, interval = range / (numResults - 1), increment = '', n = 0;
-      while (n < numResults) {
-          increment = minScore + interval * n;
-          if (totalScore <= increment) {
-              resultsTitle.replaceWith('<h1>' + resultOptions[n].title + '</h1>');
-              resultsDesc.replaceWith('<p class=\'desc\'>' + resultOptions[n].desc + '</p>');
-              return;
-          } else {
-              n++;
-          }
-      }
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const quizSteps = $('#quizzie .quiz-step');
+// let totalScore = 0;
+
+// quizSteps.each(function() {
+// const currentStep = $(this);
+// const ansOpts = currentStep.children('.quiz-answer');
+
+// ansOpts.each(function() {
+// const eachOpt = $(this);
+
+// eachOpt.on('click', check);
+
+// function check() {
+//   const $this = $(this);
+//   const value = $this.attr('data-quizIndex');
+//   const answerScore = parseInt(value);
+
+//   if (currentStep.children('.active').length > 0) {
+//     const wasActive = currentStep.children('.active');
+//     const oldScoreValue = wasActive.attr('data-quizIndex');
+//     const oldScore = parseInt(oldScoreValue);
+
+//     currentStep.children('.active').removeClass('active');
+//     $this.addClass('active');
+//     totalScore -= oldScoreValue;
+//     totalScore += answerScore;
+//     calcResults(totalScore);
+//   } else {
+//     $this.addClass('active');
+//     totalScore += answerScore;
+//     calcResults(totalScore);
+//     updateStep(currentStep);
+//   }
+// }
+// });
+// });
+
+// function updateStep(currentStep) {
+// if (currentStep.hasClass('current')) {
+// currentStep.removeClass('current');
+// currentStep.next().addClass('current');
+// }
+// }
+
+// function calcResults(totalScore) {
+// if (quizSteps.find('.active').length == quizSteps.length) {
+// const resultsTitle = $('#results h1');
+// const resultsDesc = $('#results .desc');
+// const lowestScoreArray = $('#quizzie .low-value').map(function() {
+// return $(this).attr('data-quizIndex');
+// }).get();
+// const minScore = lowestScoreArray.reduce((accumulator, currentValue) => accumulator + parseInt(currentValue), 0);
+// const highestScoreArray = $('#quizzie .high-value').map(function() {
+//     return $(this).attr('data-quizIndex');
+//   }).get();
+//   const maxScore = highestScoreArray.reduce((accumulator, currentValue) => accumulator + parseInt(currentValue), 0);
+  
+//   const range = maxScore - minScore;
+//   const numResults = resultOptions.length;
+//   const interval = range / (numResults - 1);
+//   let increment = '';
+//   let n = 0;
+  
+//   while (n < numResults) {
+//     increment = minScore + interval * n;
+  
+//     if (totalScore <= increment) {
+//       resultsTitle.replaceWith('<h1>' + resultOptions[n].title + '</h1>');
+//       resultsDesc.replaceWith('<p class=\'desc\'>' + resultOptions[n].desc + '</p>');
+//       return;
+//     } else {
+//       n++;
+//     }
+//   }
+
+// }
+// }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var quizSteps = $('#quizzie .quiz-step'), totalScore = 0;
+// quizSteps.each(function () {
+//   var currentStep = $(this), ansOpts = currentStep.children('.quiz-answer');
+//   ansOpts.each(function () {
+//       var eachOpt = $(this);
+//       eachOpt[0].addEventListener('click', check, false);
+//       function check() {
+
+//           var $this = $(this), value = $this.attr('data-quizIndex'), answerScore = parseInt(value);
+//           if (currentStep.children('.active').length > 0) {
+//               var wasActive = currentStep.children('.active'), oldScoreValue = wasActive.attr('data-quizIndex'), oldScore = parseInt(oldScoreValue);
+//               currentStep.children('.active').removeClass('active');
+//               $this.addClass('active');
+//               totalScore -= oldScoreValue;
+//               totalScore += answerScore;
+//               calcResults(totalScore);
+//           } else {
+//               $this.addClass('active');
+//               totalScore += answerScore;
+//               calcResults(totalScore);
+//               updateStep(currentStep);
+//           }
+//       }
+//   });
+// });
+// function updateStep(currentStep) {
+//   if (currentStep.hasClass('current')) {
+//       currentStep.removeClass('current');
+//       currentStep.next().addClass('current');
+//   }
+// }
+// function calcResults(totalScore) {
+//   if (quizSteps.find('.active').length == quizSteps.length) {
+//       var resultsTitle = $('#results h1'), resultsDesc = $('#results .desc');
+//       var lowestScoreArray = $('#quizzie .low-value').map(function () {
+//           return $(this).attr('data-quizIndex');
+//       });
+//       var minScore = 0;
+//       for (var i = 0; i < lowestScoreArray.length; i++) {
+//           minScore += lowestScoreArray[i];
+//       }
+//       var highestScoreArray = $('#quizzie .high-value').map(function () {
+//           return $(this).attr('data-quizIndex');
+//       });
+//       var maxScore = 0;
+//       for (var i = 0; i < highestScoreArray.length; i++) {
+//           maxScore += highestScoreArray[i];
+//       }
+//       var range = maxScore - minScore;
+//       var numResults = resultOptions.length, interval = range / (numResults - 1), increment = '', n = 0;
+//       while (n < numResults) {
+//           increment = minScore + interval * n;
+//           if (totalScore <= increment) {
+//               resultsTitle.replaceWith('<h1>' + resultOptions[n].title + '</h1>');
+//               resultsDesc.replaceWith('<p class=\'desc\'>' + resultOptions[n].desc + '</p>');
+//               return;
+//           } else {
+//               n++;
+//           }
+//       }
+//   }
+// }
